c6247889416f0628fed93ea3972eef7fb2e008e3
sandeep reddy <sandeepreddy1625@gmail.com>
2022-08-18 19:37:25 +0530

    create_user_table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_12_111519) do
+ActiveRecord::Schema.define(version: 2022_08_18_134747) do
 
   create_table "articles", force: :cascade do |t|
     t.string "tittle"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 2022_08_12_111519) do
     t.string "movie_name"
     t.string "hero"
     t.string "director"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
