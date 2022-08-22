b802c98271eb87e07408818cbe8d6bf34efd8c65
sandeep reddy <sandeepreddy1625@gmail.com>
2022-08-22 23:11:53 +0530

    added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "pages#home"
   resources :articles
   resources :movies,only: [:show,:index,:create,:new,:update,:edit,:destroy]
+  get 'signup', to: "users#new"
 end
 
